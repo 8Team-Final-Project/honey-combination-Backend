760fb01ef65230f42e8f796147c1d185532f269f
seonung_park <psw1414@naver.com>
2021-10-28 19:40:39 +0900

    경로설정완료

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+// import User from "../models/User";
+// import bcrypt from "bcrypt";
+// import jwtToken from "jsonwebtoken";
+
+export const logincheck = (req,res) => {res.send("logincheck")};
+export const signup = (req,res) => res.send("signup");
+export const checkemail = (req,res) => res.send("checkemail");
+export const checknick = (req,res) => res.send("checknick");
+export const login = (req,res) => res.send("login");
+export const logout = (req,res) => res.send("logout");
+export const me = (req,res) => res.send("me");
+export const profilepatch = (req,res) => res.send("profilepatch");
+export const quitme = (req,res) => res.send("quitme");
