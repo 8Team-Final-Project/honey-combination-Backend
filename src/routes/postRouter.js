f5c9cac606e5df024944affdb9a7fe1c123476ea
Judongjae <wnehdwo0982@naver.com>
2021-11-05 20:13:08 +0900

    좋아요시도

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -21,7 +21,7 @@ postRouter.get("/", postlist);
 postRouter.get("/event1list", event1list);
 postRouter.get("/event2list", event2list);
 postRouter.get("/event3list", event3list);
-postRouter.get("/:postid", postfind);
+postRouter.get("/:postid", authMiddleware, postfind);
 postRouter.patch(
   "/postupdate/:postid",
   authMiddleware,
@@ -34,7 +34,7 @@ postRouter.patch(
   authorCheck,
   postdelete
 );
-// postRouter.post("/uploadimg", uploadSingle.single("postImg"), postuploadimg);
+postRouter.post("/uploadimg", uploadSingle.single("postImg"), postuploadimg);
 // /image
 //단일 url 만들고 이미지 리턴 post이미지에 그대로 url req로 다시 돌려 받는다.
 export default postRouter;
